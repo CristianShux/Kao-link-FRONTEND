28b64d3f3a52704b99c5917573c75cde75fa7163
Cristian Jurajuria <cljurajuria23@gmail.com>
2025-11-18 02:24:21 -0300

    cambio cambio

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,8 @@ import axios from "axios";
 
 const API_URL= "https://kao-link-backend-gestion.onrender.com";
 
-export const WS_URL = "https://kaolinkwebsocket.duckdns.org/ws/"; // URL del WebSocket
+//export const WS_URL = "https://kaolinkwebsocket.duckdns.org/ws/"; // URL del WebSocket
+export const WS_URL = "wss://kaolinkwebsocket.duckdns.org/ws"; // URL del WebSocket
 
 // Instancia de Axios configurada
 const api = axios.create({
